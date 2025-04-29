0c368874ea548563bd224a27e6e5888a555b849a
O01o <77559448+O01o@users.noreply.github.com>
2025-04-30 00:10:18 +0900

    rye to uc

--- modules/aws/ml/data/ec2_user_data.sh
+++ modules/aws/ml/data/ec2_user_data.sh
@@ -17,15 +17,15 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
 sudo yum update -y
 sudo yum install -y git
 
-# Install Rye and MLflow
-curl -sSfL https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
-source "/root/.rye/env"
-rye init
-rye add mlflow
-rye sync
+# Install uv and MLflow
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source "/home/ec2-user/.cargo/bin"
+uv init
+uv add mlflow
+uv sync
 
 # Clone Git Project
 git clone https://github.com/O01o/pytorch_mlflow_sample.git
 cd pytorch_mlflow_sample/
-rye sync
-# rye run python train.py x.x.x.x 5000
+uv sync
+# uv run python train.py x.x.x.x 5000
--- modules/aws/tracker/data/ec2_user_data.sh
+++ modules/aws/tracker/data/ec2_user_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Install Rye and MLflow
-curl -sSfL https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
-source "/home/ec2-user/.rye/env"
-rye init
-rye add mlflow
-rye sync
+# Install uv and MLflow
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source "/home/ec2-user/.cargo/bin"
+uv init
+uv add mlflow
+uv sync
 
 # Start MLflow Tracking Server
-rye run mlflow server --host 0.0.0.0 --port 5000
+uv run mlflow server --host 0.0.0.0 --port 5000
